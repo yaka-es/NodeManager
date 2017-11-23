8478801f20cee5a0c0680dfb2c919b3720d9d0fa
Alexander Samarin <sasha@enikasoft.ru>
2017-11-24 00:56:27 +0300

    fix include paths and code style of JSONDB

--- JSONDB/JSONDB.cpp
+++ JSONDB/JSONDB.cpp
@@ -1,37 +1,34 @@
-/*
-* Copyright 2013, 2014 Range Networks, Inc.
-*
-* This software is distributed under the terms of the GNU Affero Public License.
-* See the COPYING file in the main directory for details.
-*
-* This use of this software may be subject to additional restrictions.
-* See the LEGAL file in the main directory for details.
+/* JSONDB/JSONDB.cpp */
+/*-
+ * Copyright 2013, 2014 Range Networks, Inc.
+ *
+ * This software is distributed under the terms of the GNU Affero Public License.
+ * See the COPYING file in the main directory for details.
+ *
+ * This use of this software may be subject to additional restrictions.
+ * See the LEGAL file in the main directory for details.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Affero General Public License for more details.
-
-	You should have received a copy of the GNU Affero General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
-
-#include "sqlite3util.h"
-#include "sqlite3.h"
-#include "JSONDB.h"
-
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdio.h>
+
 #include <sstream>
 
-
+#include "JSONDB.h"
 
 bool JSONDB::connect(std::string path)
 {
@@ -103,14 +100,14 @@ JsonBox::Object JSONDB::read(std::string query, unsigned retries)
 	int state;
 	int col;
 	int colMax = sqlite3_column_count(stmt);
-	const char* colName;
-	const char* colText;
+	const char *colName;
+	const char *colText;
 	while (1) {
 		state = sqlite3_step(stmt);
 		if (state == SQLITE_ROW) {
 			for (col = 0; col < colMax; col++) {
-				colName = (const char*)sqlite3_column_name(stmt, col);
-				colText = (const char*)sqlite3_column_text(stmt, col);
+				colName = (const char *)sqlite3_column_name(stmt, col);
+				colText = (const char *)sqlite3_column_text(stmt, col);
 				if (colText == NULL) {
 					o[std::string(colName)] = JsonBox::Value();
 				} else {
@@ -119,7 +116,7 @@ JsonBox::Object JSONDB::read(std::string query, unsigned retries)
 			}
 		} else if (state == SQLITE_DONE) {
 			break;
-		// TODO : handle more SQLITE_FAILURECODES here
+			// TODO : handle more SQLITE_FAILURECODES here
 		} else {
 			response["code"] = JsonBox::Value(406);
 			response["data"] = JsonBox::Value("query run failed");
@@ -191,18 +188,19 @@ JsonBox::Object JSONDB::query(JsonBox::Object request, unsigned retries)
 		response["code"] = JsonBox::Value(406);
 		response["data"] = JsonBox::Value("missing table");
 		return response;
-	} else if (table.compare("sip_buddies") == 0 || table.compare("dialdata_table") == 0 || table.compare("rates") == 0 || table.compare("subscribers") == 0) {
+	} else if (table.compare("sip_buddies") == 0 || table.compare("dialdata_table") == 0 ||
+		   table.compare("rates") == 0 || table.compare("subscribers") == 0) {
 		dbPath = "/var/lib/asterisk/sqlite3dir/sqlite3.db";
-	// TODO : should query OpenBTS directly because this table may dissappear
+		// TODO : should query OpenBTS directly because this table may dissappear
 	} else if (table.compare("neighbor_table") == 0) {
 		dbPath = "/var/run/NeighborTable.db";
-	// TODO : should query OpenBTS directly because this table may dissappear
+		// TODO : should query OpenBTS directly because this table may dissappear
 	} else if (table.compare("phystatus") == 0) {
 		dbPath = "/var/run/ChannelTable.db";
-	// TODO : should query OpenBTS directly because this table may dissappear
+		// TODO : should query OpenBTS directly because this table may dissappear
 	} else if (table.compare("tmsi_table") == 0) {
 		dbPath = "/var/run/TMSITable.db";
-	// TODO : should query OpenBTS directly because this table may dissappear
+		// TODO : should query OpenBTS directly because this table may dissappear
 	} else if (table.compare("transaction_table") == 0) {
 		dbPath = "/var/run/TransactionTable.db";
 	} else if (table.compare("spectrum_map") == 0) {
@@ -271,11 +269,13 @@ JsonBox::Object JSONDB::query(JsonBox::Object request, unsigned retries)
 			JsonBox::Object responseDIAL;
 
 			queryTMP << "insert into sip_buddies (username, name, callerid, ki, host, allow, ipaddr)";
-			queryTMP << " values (\"" << imsi << "\",\"" << name << "\",\"" << msisdn << "\",\"" << ki << "\",\"dynamic\",\"gsm\",\"127.0.0.1\")";
+			queryTMP << " values (\"" << imsi << "\",\"" << name << "\",\"" << msisdn << "\",\"" << ki
+				 << "\",\"dynamic\",\"gsm\",\"127.0.0.1\")";
 			responseSIP = execOnly(queryTMP.str());
 
 			queryTMP.str("");
-			queryTMP << "insert into dialdata_table (dial, exten) values (\"" << imsi << "\",\"" << msisdn << "\")";
+			queryTMP << "insert into dialdata_table (dial, exten) values (\"" << imsi << "\",\"" << msisdn
+				 << "\")";
 			responseDIAL = execOnly(queryTMP.str());
 
 			if (responseSIP["code"].getInt() == 200 && responseDIAL["code"].getInt() == 200) {
@@ -288,7 +288,9 @@ JsonBox::Object JSONDB::query(JsonBox::Object request, unsigned retries)
 			}
 
 		} else if (action.compare("read") == 0) {
-			q << "select sip_buddies.username as \"imsi\", sip_buddies.name as \"name\", dialdata_table.exten as \"msisdn\" from sip_buddies left outer join dialdata_table on sip_buddies.username = dialdata_table.dial";
+			q << "select sip_buddies.username as \"imsi\", sip_buddies.name as \"name\", "
+			     "dialdata_table.exten as \"msisdn\" from sip_buddies left outer join dialdata_table on "
+			     "sip_buddies.username = dialdata_table.dial";
 			return read(q.str());
 
 		} else if (action.compare("update") == 0) {
@@ -324,11 +326,13 @@ JsonBox::Object JSONDB::query(JsonBox::Object request, unsigned retries)
 			JsonBox::Object responseSIP;
 			JsonBox::Object responseDIAL;
 
-			queryTMP << "update sip_buddies set name=\"" << name << "\", callerid=\"" << msisdn << "\" where username=\"" << imsi << "\"";
+			queryTMP << "update sip_buddies set name=\"" << name << "\", callerid=\"" << msisdn
+				 << "\" where username=\"" << imsi << "\"";
 			responseSIP = execOnly(queryTMP.str());
 
 			queryTMP.str("");
-			queryTMP << "update dialdata_table set exten=\"" << msisdn << "\" where dial=\"" << imsi << "\"";
+			queryTMP << "update dialdata_table set exten=\"" << msisdn << "\" where dial=\"" << imsi
+				 << "\"";
 			responseDIAL = execOnly(queryTMP.str());
 
 			if (responseSIP["code"].getInt() == 200 && responseDIAL["code"].getInt() == 200) {
@@ -370,10 +374,9 @@ JsonBox::Object JSONDB::query(JsonBox::Object request, unsigned retries)
 				response["data"]["sip_buddies"] = responseSIP;
 				response["data"]["dialdata_table"] = responseDIAL;
 			}
-
 		}
 
-	// CREATE
+		// CREATE
 	} else if (action.compare("create") == 0) {
 		// start query
 		q << "insert into " << table;
@@ -396,11 +399,12 @@ JsonBox::Object JSONDB::query(JsonBox::Object request, unsigned retries)
 			colNames.push_back(jit->first);
 			colValues.push_back(jit->second.getString());
 		}
-		q << " (" << implode(",", colNames) << ") values (" << "\"" << implode("\",\"", colValues) << "\")";
+		q << " (" << implode(",", colNames) << ") values ("
+		  << "\"" << implode("\",\"", colValues) << "\")";
 
 		return execOnly(q.str());
 
-	// READ
+		// READ
 	} else if (action.compare("read") == 0) {
 		// start query
 		q << "select * from " << table;
@@ -410,7 +414,7 @@ JsonBox::Object JSONDB::query(JsonBox::Object request, unsigned retries)
 
 		return read(q.str());
 
-	// UPDATE
+		// UPDATE
 	} else if (action.compare("update") == 0) {
 		// start query
 		q << "update " << table << " set";
@@ -441,7 +445,7 @@ JsonBox::Object JSONDB::query(JsonBox::Object request, unsigned retries)
 
 		return execOnly(q.str());
 
-	// DELETE
+		// DELETE
 	} else if (action.compare("delete") == 0) {
 		// start query
 		q << "delete from " << table;
